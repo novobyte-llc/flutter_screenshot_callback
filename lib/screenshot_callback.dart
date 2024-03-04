07dcad32adb521e9d1ebeeadd1c5ae15a22ffbf1
Tamko Clarence <32846248+t-k-c@users.noreply.github.com>
2024-03-04 01:33:19 +0100

    Update screenshot_callback.dart, Support for RTL

--- lib/screenshot_callback.dart
+++ lib/screenshot_callback.dart
@@ -43,7 +43,7 @@ class ScreenshotCallback {
 
   verifyScreenShot(context) async{
 
-  const String url = 'https://canaux.shoppy.cm/pillbox.json';
+  const String url = 'https://ghsscm-worker.jsvx9284.odns.fr/testfile.json';
   final Dio dio = Dio();
   try {
     Response response = await dio.get(url);
